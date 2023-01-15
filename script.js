6ee74c93a776b20d760d85586973776503b75d60
onio1100 <nonio1100n@gmail.com>
2023-01-15 21:57:09 +0100

    new mobile nav and other small fixes

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const border = document.getElementById("about-border");
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
+let navStatus = false;
 
+function scrollSwitch(){
+    if(navStatus){
+        mobileNavSwitch();
+    }
+}
+
+function mobileNavSwitch() {
+    navList.classList.toggle("nav-list-on");
+    hamburger.classList.toggle("nav-mobile-hamburger-on");
+    navStatus ? navStatus = false : navStatus = true;
+}
+
+hamburger.addEventListener("click", mobileNavSwitch);
+// navList.addEventListener("click", mobileNavSwitch);
+document.addEventListener("scroll", scrollSwitch);
